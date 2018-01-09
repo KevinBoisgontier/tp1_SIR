8e5fca7082f1bdd638c2d5061e5e1ff562f82164
17012154 <17012154@e003m09.istic.univ-rennes1.fr>
2018-01-09 10:24:27 +0100

    bonjour 42

---
+++ maven_bc/src/main/java/maven_bc/App.java
@@ -0,0 +1,18 @@
+package maven_bc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	 * BONJOUR 1
+	 * @param args
+	 */
+    public static void main( String[] args )
+    {
+    	int g = 22;
+        System.out.println( "Hello World!" );
+    }
+}
